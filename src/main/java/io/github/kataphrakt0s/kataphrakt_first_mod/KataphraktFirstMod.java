e4b06dc6c1d2e2ddb6912e8061e20f45e0f6f6a8
kataphrakt0s <cam.m.muir@gmail.com>
2024-03-17 11:13:25 -0600

    attempting to add an item

--- src/main/java/io/github/kataphrakt0s/kataphrakt_first_mod/KataphraktFirstMod.java
+++ src/main/java/io/github/kataphrakt0s/kataphrakt_first_mod/KataphraktFirstMod.java
@@ -1,7 +1,15 @@
 package io.github.kataphrakt0s.kataphrakt_first_mod;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.impl.qsl.item.v1.QuiltItemCompatMod;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.item.ItemGroups;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,8 +19,17 @@ public class KataphraktFirstMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Kataphrakt0s' First Mod");
 
+	public static final Item KATAPHRAKT_ITEM = new Item(new QuiltItemSettings());
+
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "kataphrakt_item"), KATAPHRAKT_ITEM);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+			entries.addItem(KATAPHRAKT_ITEM);
+		});
 	}
+
 }
